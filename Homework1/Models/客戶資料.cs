52184dc8bab9549f1d9fdd558dad94f21fc7ca0f
chen-xiny-i <xinyi_chen@lativ.com.tw>
2015-07-11 02:21:16 +0800

    * 對於 Create 與 Edit 表單要套用欄位驗證 (必填、Email格式、欄位長度限制等驗證) * 刪除資料功能不能真的刪除資料庫中的資料，必須修改資料庫，加上一個「是否已刪除」欄位，資料庫中的該欄位為 bit 類型，0 代表未刪除，1 代表已刪除，且列表頁不得顯示已刪除的資料。 * 新增或更新客戶資料時，需驗證「手機」的電話格式必須為 "\d{4}-\d{6}" 的格式 ( e.g. 0911-111111 )

--- Homework1/Models/客戶資料.cs
+++ Homework1/Models/客戶資料.cs
@@ -11,7 +11,8 @@ namespace Homework1.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class 客戶資料
     {
         public 客戶資料()
@@ -19,15 +20,22 @@ namespace Homework1.Models
             this.客戶銀行資訊 = new HashSet<客戶銀行資訊>();
             this.客戶聯絡人 = new HashSet<客戶聯絡人>();
         }
-    
+
         public int Id { get; set; }
+        [Required]
         public string 客戶名稱 { get; set; }
+        [Required]
         public string 統一編號 { get; set; }
+        [Required]
+        [RegularExpression(@"[0-9]{4}-[0-9]{6}", ErrorMessage = "電話格式錯誤")]
         public string 電話 { get; set; }
         public string 傳真 { get; set; }
         public string 地址 { get; set; }
+        [Required]
+        [RegularExpression(@"[A-Za-z0-9_.]+@[A-Za-z0-9_.]+", ErrorMessage = "Email格式錯誤")]
         public string Email { get; set; }
-    
+        public bool 是否已刪除 { get; set; }
+
         public virtual ICollection<客戶銀行資訊> 客戶銀行資訊 { get; set; }
         public virtual ICollection<客戶聯絡人> 客戶聯絡人 { get; set; }
     }
