ddb6080230f0db0c5cb4a7f721c34409c5fb70e2
chen-xiny-i <xinyi_chen@lativ.com.tw>
2015-07-11 01:31:59 +0800

    實作一份簡易報表，顯示「客戶名稱、聯絡人數量、銀行帳戶數量」共三個欄位

---
+++ Homework1/Controllers/CustomerReportController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Homework1.Models;
+
+
+namespace Homework1.Controllers
+{
+    public class CustomerReportController : Controller
+    {
+        private 客戶資料Entities db = new 客戶資料Entities();
+
+        // GET: CustomerReport
+        public ActionResult Index()
+        {
+            return View(db.ViewCustomerAmtReport.ToList());
+        }
+    }
+}
